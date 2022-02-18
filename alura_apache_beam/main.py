d2cbfa8b0992697da5b9e89e92949b3abdb1e5a2
julia_rolemberg <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>
2022-02-18 15:58:44 -0300

    pipeline: list to dict

--- alura_apache_beam/main.py
+++ alura_apache_beam/main.py
@@ -4,6 +4,7 @@ from apache_beam.io import ReadFromText
 
 pipeline_options =  PipelineOptions(argv=None)
 pipeline  = beam.Pipeline(options=pipeline_options)
+colunas_dengue = ["id","data_iniSE","casos","ibge_code","cidade","uf","cep","latitude","longitude"]
 
 def texto_para_lista(elemento, delimitador='|'):
     """
@@ -12,11 +13,17 @@ def texto_para_lista(elemento, delimitador='|'):
     """
     return elemento.split(delimitador)
 
-
+def lista_para_dict(elemento, colunas):
+    """
+    Recebe 2 listas
+    Retorna um dicionário
+    """
+    return dict(zip(colunas, elemento))
 dengue = (
     pipeline
     | "Leitura do dataset de dengue" >> ReadFromText('data/casos_dengue.txt', skip_header_lines=1)
     | "De  texto para lista" >> beam.Map(texto_para_lista)
+    | "De lista para dicionário" >> beam.Map(lista_para_dict, colunas_dengue)
     | "Mostrar Resultados" >> beam.Map(print)
 )
 
