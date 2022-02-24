a4d89705ff049731edda118a44c369ff948ece5f
julia_rolemberg <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>
2022-02-23 21:22:41 -0300

    groupby key

--- alura_apache_beam/main.py
+++ alura_apache_beam/main.py
@@ -19,12 +19,33 @@ def lista_para_dict(elemento, colunas):
     Retorna um dicionário
     """
     return dict(zip(colunas, elemento))
+
+def trata_data(elemento):
+    """
+    Recebe um dicionário e cria um novo campo com ANO-MES
+    Retorna o mesmo dicionário com o novo campo
+    """
+    elemento['ano_mes'] = '-'.join(elemento['data_iniSE'].split('-')[:2])
+    return elemento
+
+def chave_uf(elemento):
+    """
+    Recebe um dicionário
+    Retorna uma tupla(UF, dicionário)
+    """
+    chave = elemento['uf']
+    return (chave, elemento)
+    
 dengue = (
     pipeline
     | "Leitura do dataset de dengue" >> ReadFromText('data/casos_dengue.txt', skip_header_lines=1)
     | "De  texto para lista" >> beam.Map(texto_para_lista)
     | "De lista para dicionário" >> beam.Map(lista_para_dict, colunas_dengue)
+    | "Criar campo ano_mes" >> beam.Map(trata_data)
+    | "Criar chave pelo estado" >> beam.Map(chave_uf)
+    | "Agrupar pelo estado" >> beam.GroupByKey()
     | "Mostrar Resultados" >> beam.Map(print)
+    
 )
 
 pipeline.run()
