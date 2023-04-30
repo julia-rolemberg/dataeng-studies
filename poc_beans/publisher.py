daae9635d0f398197e154ae69099ff3336ef294f
julia-rolemberg <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>
2023-04-30 18:57:20 -0300

    add poc

--- poc_beans/publisher.py
+++ poc_beans/publisher.py
@@ -1,5 +1,12 @@
 import random
 import time
+from google.cloud import pubsub_v1
+
+project_id = "espm-dataops-2023"
+topic_id = "testing-dataflow"
+
+publisher = pubsub_v1.PublisherClient()
+topic_path = publisher.topic_path(project_id, topic_id)
 
 # Define the attributes and the time to run the code (in seconds)
 attributes = ['a', 'b', 'c', 'd', 'e']
@@ -12,6 +19,9 @@ def generate_random_numbers():
 # Generate and output one individual every half second
 start_time = time.time()
 while time.time() - start_time < run_time:
-    individual = {attr: val for attr, val in zip(attributes, generate_random_numbers())}
-    print(individual)
+    individual_str = str({attr: val for attr, val in zip(attributes, generate_random_numbers())})
+    print(individual_str)
+    individual = individual_str.encode("utf-8")
+    future = publisher.publish(topic_path, individual)
+    print(future.result())
     time.sleep(0.5)
