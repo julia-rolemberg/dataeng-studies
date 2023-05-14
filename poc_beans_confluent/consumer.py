17f244e8eb03d8ca7a32ed3680c9984cf5d550f9
julia-rolemberg <61888090+julia-rolemberg@users.noreply.github.com>
2023-05-14 12:30:00 -0300

    poc_beans_confluent:add consumer and producer confluent

---
+++ poc_beans_confluent/consumer.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+import sys
+from argparse import ArgumentParser, FileType
+from configparser import ConfigParser
+import json
+from confluent_kafka import Consumer, OFFSET_BEGINNING, KafkaError
+
+if __name__ == '__main__':
+    # Parse the command line.
+    parser = ArgumentParser()
+    parser.add_argument('config_file', type=FileType('r'))
+    parser.add_argument('--reset', action='store_true')
+    args = parser.parse_args()
+
+    # Parse the configuration.
+    # See https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
+    config_parser = ConfigParser()
+    config_parser.read_file(args.config_file)
+    config = dict(config_parser['default'])
+    config.update(config_parser['consumer'])
+
+    # Create Consumer instance
+    consumer = Consumer(config)
+
+    # Set up a callback to handle the '--reset' flag.
+    def reset_offset(consumer, partitions):
+        if args.reset:
+            for p in partitions:
+                p.offset = OFFSET_BEGINNING
+            consumer.assign(partitions)
+
+    # Subscribe to topic
+    topic = "topic_teste"
+    consumer.subscribe([topic], on_assign=reset_offset)
+
+    # Consume messages from the Kafka topic
+    try:
+        while True:
+            msg = consumer.poll(1.0)
+            if msg is None:
+                continue
+            if msg.error():
+                if msg.error().code() == KafkaError._PARTITION_EOF:
+                    print('End of partition reached')
+                else:
+                    print('Error while consuming message: {}'.format(msg.error()))
+            else:
+                message = json.loads(msg.value().decode('utf-8'))
+                size = message['size']
+                avg_speed = message['avg_speed']
+                age = message['age']
+                # Do something with the individual's characteristics
+                print(f'Size: {size}, Avg Speed: {avg_speed}, Age: {age}')
+
+    except KeyboardInterrupt:
+        pass
+
+    finally:
+        # Close the Kafka consumer instance
+        consumer.close()
---
+++ poc_beans_confluent/producer.py
@@ -0,0 +1,47 @@
+import json
+import sys
+import time
+import random
+from argparse import ArgumentParser, FileType
+from configparser import ConfigParser
+from confluent_kafka import Producer
+
+def generate_individual():
+    individual = {
+        'size': round(random.uniform(1.0, 2.5), 2),
+        'avg_speed': round(random.uniform(5.0, 20.0), 2),
+        'age': random.randint(18, 80)
+    }
+    return individual
+
+if __name__ == '__main__':
+    # Parse the command line arguments
+    parser = ArgumentParser()
+    parser.add_argument('config_file', type=FileType('r'))
+    args = parser.parse_args()
+
+    # Parse the configuration
+    config_parser = ConfigParser()
+    config_parser.read_file(args.config_file)
+    config = dict(config_parser['default'])
+
+    # Create a Kafka producer instance
+    producer = Producer(config)
+
+    # Define a delivery callback to handle produced messages
+    def delivery_callback(err, msg):
+        if err is not None:
+            print(f'Message delivery failed: {err}')
+        else:
+            print(f"Produced event to topic {msg.topic()}: message = {msg.value().decode('utf-8')}")
+
+    # Define the Kafka topic to publish messages to
+    topic = 'topic_teste'
+
+    # Continuously generate and publish messages to the Kafka topic
+    while True:
+        individual = generate_individual()
+        message = json.dumps(individual).encode('utf-8')
+        producer.produce(topic, value=message, callback=delivery_callback)
+        producer.poll(0)
+        time.sleep(1)
